96d6e34bdae5a4da2f15b45a5f3f3b029694bf1b
Tom Poulter <tom.poulter.1@outlook.com>
2020-05-10 13:17:29 +0100

    accomodation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get 'accomodation', to: "pages#accomodation", as: :accomodation
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
