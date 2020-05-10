391a9b2668845b2344099587b9c83a7c87d8d315
Tom Poulter <tom.poulter.1@outlook.com>
2020-05-10 14:58:22 +0100

    whats going in progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'accomodation', to: "pages#accomodation", as: :accomodation
   get 'wedding', to: "pages#wedding", as: :wedding
   get "local", to: "pages#local", as: :local
+  get "itinerary", to: "pages#itinerary", as: :itinerary
+  get "contact", to: "pages#contact", as: :contact
+  get "gifts", to: "pages#gifts", as: :gifts
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
