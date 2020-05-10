eed4666a40ed4e19d3a96604e6b9a047476ebf61
Tom Poulter <tom.poulter.1@outlook.com>
2020-05-10 13:26:55 +0100

    pictures added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'accomodation', to: "pages#accomodation", as: :accomodation
-
+  get 'wedding', to: "pages#wedding", as: :wedding
+  get "local", to: "pages#local", as: :local
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
