ef6a93afac1de298e45b88cdfbc3245f5a63c159
Tom Poulter <tom.poulter.1@outlook.com>
2020-06-02 20:55:16 +0100

    back to normal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -25,6 +24,8 @@ require("channels")
 // External imports
 import "bootstrap";
 
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
